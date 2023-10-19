fa9906460e234aaeb351638094083ce12b9e5c7c
praesidi <edik.halilov1999@gmail.com>
2023-10-19 20:38:12 +0300

    add basic footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import Logo from '../UI/Logo/Logo';
+import styles from './Footer.module.sass';
+
+function Footer() {
+	return (
+		<footer className={styles.container}>
+			<div className={styles.credits}>
+				<span>
+					Powered by{' '}
+					<a
+						href='https://github.com/HackerNews/API'
+						target='_blank'
+						className={styles.link}
+					>
+						Hacker News API
+					</a>
+				</span>
+				<span>
+					Designed and created by{' '}
+					<a
+						href='https://github.com/praesidi'
+						target='_blank'
+						className={styles.link}
+					>
+						praesidi
+					</a>
+				</span>
+			</div>
+			<div className={styles['logo-container']}>
+				<Logo logoHeight={20} color='white' gap={2} />
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
