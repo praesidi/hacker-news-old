832d7e31de10ab12edddda8f578e87f3258e2834
praesidi <edik.halilov1999@gmail.com>
2023-10-23 22:38:15 +0300

    add story card

---
+++ src/components/StoryCard/StoryCard.tsx
@@ -0,0 +1,14 @@
+import styles from './StoryCard.module.sass';
+
+function StoryCard() {
+	return (
+		<div className={styles.container}>
+			<div>69^</div>
+			<div>
+				<h2>title</h2>
+			</div>
+		</div>
+	);
+}
+
+export default StoryCard;
