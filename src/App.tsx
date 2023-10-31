52e307366c28bf70336e9f1ae621a1b489a91408
praesidi <edik.halilov1999@gmail.com>
2023-10-31 18:53:56 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import NotFound from './pages/NotFound/NotFound';
+import StoryPage from './pages/StoryPage/StoryPage';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='/story/:id' element={<StoryPage />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
