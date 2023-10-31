d005d075f62174a7901388e7948738372bb1c2d0
praesidi <edik.halilov1999@gmail.com>
2023-10-31 18:55:17 +0300

    rename component and move styling to sass module

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -1,19 +1,10 @@
-import { IStoryData } from '../StoryCard/StoryCard';
 import StoryCard from '../StoryCard/StoryCard';
+import { IStoryData } from '../StoryCard/StoryCard';
+import styles from './Stories.module.sass';
 
 function Stories({ data }: { data: IStoryData[] | null }) {
-	console.log(data);
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				gap: '10px',
-				marginBottom: '50px',
-				width: '100%',
-			}}
-		>
+		<div className={styles.container}>
 			<h1>100 latest stories</h1>
 			{data === null ? (
 				<h1>Nothing to see here, lol</h1>
